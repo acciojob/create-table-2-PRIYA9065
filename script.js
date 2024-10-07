546c278a830e141691100163c3904c0d525694cd
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-10-08 01:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const mytable = document.getElementById("myTable");
+
 function createTable() {
     //Write your code here
-  let rows = prompt("Input number of rows");
-  let columns = prompt("Input number of columns");
-	for(let i = 0; i < rows; i++){
-		const row = mytable.insertRow(i);
-		for(let j = 0; j < columns; j++){
-			const column = mytable.insertCell(j);
-			row.append(column);
+
+	let rows = prompt("Input number of rows");
+    let columns = prompt("Input number of columns");
+	if(columns){
+	    var mytable = document.getElementById("myTable");
+        mytable.innerHTML = ""; 
+	
+	    for(let i = 0; i < rows; i++){
+		    const row = mytable.insertRow(i);
+			for(let j = 0; j < columns; j++){
+				const column = row.insertCell(j);
+				column.innerHTML = "Row-" + (i) + "Column-" + (j);
+			}
 		}
 	}
+	
 }
